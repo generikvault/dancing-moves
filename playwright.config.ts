33078cdf245253ea337c29521aac2f8baaa12d9d
Marco Volkert <marco.volkert24@gmail.com>
2022-08-06 12:38:33 +0200

    playwright start webserver

--- playwright.config.ts
+++ playwright.config.ts
@@ -143,10 +143,10 @@ const config: PlaywrightTestConfig = {
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: 'npm run start',
+    port: 4200,
+  },
 };
 
 export default config;
