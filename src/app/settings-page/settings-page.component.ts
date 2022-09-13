7bfcc6793ce59b275e34cdffa327a4b78e457e4f
Marco Volkert <marco.volkert24@gmx.de>
2022-09-13 23:36:17 +0200

    fix #10

--- src/app/settings-page/settings-page.component.ts
+++ src/app/settings-page/settings-page.component.ts
@@ -33,13 +33,17 @@ export class SettingsPageComponent implements OnInit, OnDestroy {
     this.subscriptions.push(this.settingsForm.valueChanges.subscribe(value => {
       console.log(value);
       const queryJson = { 'secret': value.secretRead, 'secret-write': value.secretWrite, 'special-rights': value.specialRights };
-      this.navService.navigate([], queryJson);
       this.url = this.createUrl(queryJson);
       localStorage.setItem('secret', value.secretRead);
       localStorage.setItem('secret-write', value.secretWrite);
       localStorage.setItem('special-rights', value.specialRights);
       localStorage.setItem('sheetId', value.sheetId);
       this.settings.isDeveloper = value.isDeveloper;
+      if (environment.isAndroid) {
+        this.settings.initSettings({});
+      } else {
+        this.navService.navigate([], queryJson);
+      }
     }));
     this.settingsForm.patchValue({
       secretRead: this.settings.secretReadString,
