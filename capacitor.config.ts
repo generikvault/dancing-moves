ed118cd7db7554c86d3826b35d4f690576c6c039
Marco Volkert <marco.volkert24@gmx.de>
2022-09-30 21:31:09 +0200

    first steps to migrate to capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.github.mvolkert.dancingmoves',
+  appName: 'Dancing Moves',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
