51843ce55029c048fab1e9078870f01feba4b43f
Marco Volkert <marco.volkert24@gmx.de>
2023-02-26 12:05:07 +0100

    update playwright reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,5 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
-import path from 'path';
 
 /**
  * Read environment variables from file.
@@ -33,47 +32,48 @@ const config: PlaywrightTestConfig = {
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter:
     [
+      ['list'],
       ['html'],
       ['allure-playwright'],
-      [
-        '@bgotink/playwright-coverage',
-      /** @type {import('@bgotink/playwright-coverage').CoverageReporterOptions} */ {
-          // Path to the root files should be resolved from, most likely your repository root
-          sourceRoot: __dirname,
-          // Files to ignore in coverage, useful
-          // - if you're testing the demo app of a component library and want to exclude the demo sources
-          // - or part of the code is generated
-          // - or if you're running into any of the other many reasons people have for excluding files
-          exclude: ['**/node_modules/**', '**/webpack/**'],
-          // Directory in which to write coverage reports
-          resultDir: path.join(__dirname, 'results/e2e-coverage'),
-          // Configure the reports to generate.
-          // The value is an array of istanbul reports, with optional configuration attached.
-          reports: [
-            // Create an HTML view at <resultDir>/index.html
-            ['html'],
-            // Create <resultDir>/coverage.lcov for consumption by tooling
-            [
-              'lcovonly',
-              {
-                file: 'coverage.lcov',
-              },
-            ],
-            // Log a coverage summary at the end of the test run
-            [
-              'text-summary',
-              {
-                file: null,
-              },
-            ],
-          ],
-          // Configure watermarks, see https://github.com/istanbuljs/nyc#high-and-low-watermarks
-          // watermarks: {},
-          rewritePath: ({ absolutePath, relativePath }: any) => {
-            return absolutePath.replace('webpack:', '');
-          },
-        },
-      ],
+      //[
+      //  '@bgotink/playwright-coverage',
+      ///** @type {import('@bgotink/playwright-coverage').CoverageReporterOptions} */ {
+      //    // Path to the root files should be resolved from, most likely your repository root
+      //    sourceRoot: __dirname,
+      //    // Files to ignore in coverage, useful
+      //    // - if you're testing the demo app of a component library and want to exclude the demo sources
+      //    // - or part of the code is generated
+      //    // - or if you're running into any of the other many reasons people have for excluding files
+      //    exclude: ['**/node_modules/**', '**/webpack/**'],
+      //    // Directory in which to write coverage reports
+      //    resultDir: path.join(__dirname, 'results/e2e-coverage'),
+      //    // Configure the reports to generate.
+      //    // The value is an array of istanbul reports, with optional configuration attached.
+      //    reports: [
+      //      // Create an HTML view at <resultDir>/index.html
+      //      ['html'],
+      //      // Create <resultDir>/coverage.lcov for consumption by tooling
+      //      [
+      //        'lcovonly',
+      //        {
+      //          file: 'coverage.lcov',
+      //        },
+      //      ],
+      //      // Log a coverage summary at the end of the test run
+      //      [
+      //        'text-summary',
+      //        {
+      //          file: null,
+      //        },
+      //      ],
+      //    ],
+      //    // Configure watermarks, see https://github.com/istanbuljs/nyc#high-and-low-watermarks
+      //    // watermarks: {},
+      //    rewritePath: ({ absolutePath, relativePath }: any) => {
+      //      return absolutePath.replace('webpack:', '');
+      //    },
+      //  },
+      //],
     ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
